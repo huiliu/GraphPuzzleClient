3f0c979d170f59e168497012b436bb4a684bca4a
刘辉 <liuh@g-bits.com>
2018-11-15 17:59:03 +0800

    初步调整动画播放形式

--- Assets/Scripts/Client/Component/GameComponent.cs
+++ Assets/Scripts/Client/Component/GameComponent.cs
@@ -98,17 +98,57 @@ namespace GraphGame.Client
 
         private Vector2 GameBoardSizeDelta;
         private List<GameObject> PathMoveComponents = new List<GameObject>();
+        private Dictionary<Logic.Color, List<List<int>>> CurrentColorPath;
         private void OnGameSquareAck()
+        {
+            this.CurrentColorPath = this.Game.GetPlayerPath(this.CurrentPlayer);
+            this.DoDrawPath();
+
+            //var colorPath = this.Game.GetPlayerPath(this.CurrentPlayer);
+            //foreach (var kvp in colorPath)
+            //{
+            //    foreach (var points in kvp.Value)
+            //    {
+            //        if (points.Count <= 2)
+            //            continue;
+
+            //        var go = Instantiate(this.LinePathObject);
+            //        go.transform.SetParent(this.transform, false);
+            //        go.SetActive(true);
+
+            //        var LinePath = go.GetComponent<LinePathComponent>();
+            //        foreach (var idx in points)
+            //        {
+            //            var r = -1;
+            //            var c = -1;
+            //            var p = Vector2.zero;
+            //            this.Game.IndxConvertToRowCol(idx, out r, out c);
+            //            p.x = c;
+            //            p.y = -r;
+            //            LinePath.AppendNode(origin + p * 16);
+            //        }
+
+            //        LinePath.OnArriveNode += this.OnArriveNode;
+            //        LinePath.Run();
+            //    }
+            //}
+        }
+
+        private void DoDrawPath()
         {
             this.GameBoardSizeDelta = this.GameBoardRect.sizeDelta;
             var origin = this.GameBoardSizeDelta / 2;
             origin.x = -origin.x;
 
-            var colorPath = this.Game.GetPlayerPath(this.CurrentPlayer);
-            foreach (var kvp in colorPath)
+            var curColor = Logic.Color.None;
+            var pathNum = -1;
+
+            foreach (var kvp in this.CurrentColorPath)
             {
-                foreach (var points in kvp.Value)
+                curColor = kvp.Key;
+                for (var i = 0; i < kvp.Value.Count; ++i) //points in kvp.Value)
                 {
+                    var points = kvp.Value[i];
                     if (points.Count <= 2)
                         continue;
 
@@ -130,8 +170,17 @@ namespace GraphGame.Client
 
                     LinePath.OnArriveNode += this.OnArriveNode;
                     LinePath.Run();
+
+                    pathNum = i;
+                    break;
                 }
+
+                if (pathNum != -1)
+                    break;
             }
+
+            if (curColor != Logic.Color.None && pathNum != -1)
+                this.CurrentColorPath[curColor].RemoveAt(pathNum);
         }
 
         private void OnArriveNode(PathMoveComponent pathMoveComponent, int idx)
@@ -140,6 +189,8 @@ namespace GraphGame.Client
             {
                 pathMoveComponent.OnArriveNode -= this.OnArriveNode;
                 Destroy(pathMoveComponent.gameObject);
+
+                this.DoDrawPath();
             }
         }
     }
