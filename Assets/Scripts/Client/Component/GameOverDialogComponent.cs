a2894604017eeb2acb079b3de7b04e3c5735f071
刘辉 <liuh@g-bits.com>
2018-11-15 17:11:21 +0800

    取消结束对话框点击关闭功能
    调整目标结构

--- Assets/Scripts/Client/Component/GameOverDialogComponent.cs
+++ Assets/Scripts/Client/Component/GameOverDialogComponent.cs
@@ -10,18 +10,12 @@ namespace GraphGame.Client
 {
     public class GameOverDialogComponent
         : MonoBehaviour
-        , IPointerDownHandler
     {
         [SerializeField] private Text FinalScoreText;
 
         public void SetScore(int s)
         {
             this.FinalScoreText.text = s.ToString();
-        }
-
-        public void OnPointerDown(PointerEventData eventData)
-        {
-            this.gameObject.SetActive(false);
         }
     }
 }
