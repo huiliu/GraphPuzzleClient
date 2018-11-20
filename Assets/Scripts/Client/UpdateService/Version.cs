9b5afce700c5ebf9cccdfd6e6b8083c13b8cba27
刘辉 <liuh@g-bits.com>
2018-11-20 17:58:12 +0800

    增加更新资源

---
+++ Assets/Scripts/Client/UpdateService/Version.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphGame.Client
+{
+    public class Version
+    {
+        public static int Major = 0;
+        public static int Minor = 1;
+        public static int Patch = 0;
+
+        private string version;
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(this.version))
+            {
+                this.version = string.Format("{0}.{1}.{2}", Major, Minor, Patch);
+            }
+
+            return this.version;
+        }
+    }
+}
